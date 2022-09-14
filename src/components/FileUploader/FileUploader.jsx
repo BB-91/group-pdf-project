37b4abbebf91b019549fc679975a294827646f66
Ben Benoit <benoit.ben.j@gmail.com>
2022-09-13 23:06:45 -0500

    Added form fields. Changed Sequelize model

--- src/components/FileUploader/FileUploader.jsx
+++ src/components/FileUploader/FileUploader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./FileUploader.scss";
+import { STATE_OPTIONS_ELEMENTS } from "../../data/alphabeticalStates.mjs";
 
 const FileUploader = () => {
 
@@ -8,12 +9,29 @@ const FileUploader = () => {
         console.log("Submit button clicked.")
     }
 
+    // body: {firstName: <str>, lastName: <str>, city: <str>, state: <str>, zipCode: <int>, keywords: <arr?>,}
+
     return (
         <div className='file-uploader'>
-            <p>Upload a profile PDF</p>
+            <h5>Upload a profile PDF</h5>
             <form id='file-upload-form'>
                 <input type="file" name="file-input" id="file-input" accept=".pdf" />
-                <input type="text" name="file-comments" id="file-comments" placeholder="File Comments" />
+                
+                <div id="name-row" className="row">
+                    <input type="text" name="first-name" id="first-name" placeholder="First Name" required />
+                    <input type="text" name="last-name" id="last-name" placeholder="Last Name" required />
+                </div>
+
+                <div id="location-row" className="row">
+                    <input type="text" name="city" id="city" placeholder="City" required />
+                    <select name="state" id="state" required>
+                        <option disabled={false} value=""> State </option>
+                        {STATE_OPTIONS_ELEMENTS}
+                    </select>
+                    <input type="text" name="zip-code" id="zip-code" placeholder="Zip Code" required />
+                </div>
+
+                <input type="text" name="keywords" id="keywords" placeholder="Keywords (comma-separated)" />
                 <button type="submit" onClick={handleSubmitButtonClick}>Upload File</button>
             </form>
         </div>
