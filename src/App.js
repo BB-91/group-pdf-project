46f6bdb6ad192d4879fef447078ed8e691f433d8
samiistout <samuelstout6580@gmail.com>
2022-09-27 14:24:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import FileUploader from './components/FileUploader/FileUploader';
 import SearchResults from './containers/SearchResults/SearchResults';
 import UploadForm from './containers/UploadForm/UploadForm';
 
+let 
+
 function App() {
     return (
         <div className="App">
---
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,19 @@
+import "./ShoppingCart.scss"
+import { useState } from "react"
+
+const ShoppingCart = (props) =>{
+
+    const [showCart, setShowCart] = useState()
+
+////container for each profile line
+//render with button to take out of cart
+// togle 
+
+
+    return (
+        <button>Cart</button>
+
+    )
+}
+
+export default ShoppingCart
