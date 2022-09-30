d887b98cded513d0ee3557faa73b0307342f733b
Eddison Chen <eddisonchen1@gmail.com>
2022-09-30 17:49:43 -0400

    toggles add and remove to cart button

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,24 +1,34 @@
+import {useState} from 'react';
+
 const ProfileCard = (props) =>{
     const {ShoppingCartArr, setShoppingCartArr, id, file, city, state, zipCode, country, cohort_year, firstName, lastName } = props;
+
+    const [removeButton, setRemoveButton] = useState(false)
 
     const addToArray = () => {
         setShoppingCartArr(ShoppingCartArr => [...ShoppingCartArr, file])
         console.log("added to array")
+        setRemoveButton(!removeButton)
     }
 
     const removeFromArray = (event) => {
         const profileId = event.target.getAttribute("id")
         setShoppingCartArr(ShoppingCartArr.filter(profile => profile.id != profileId))
+        setRemoveButton(!removeButton)
     }
+
+    const addCard = (<button onClick={addToArray}>add to cart</button>)
+    const removeCard = (<button id={id} onClick={removeFromArray}>Remove</button>)
 
 return (
     <>
-                <p>{firstName} {lastName}</p>
-                <p>{city}, {state}, {zipCode}, {country}</p>
-                <p>{cohort_year}</p>
-                <p>{id}</p>
-                <button onClick={addToArray}>add to cart</button>
-                <button id={id} onClick={removeFromArray}>Remove</button>
+        <p>{firstName} {lastName}</p>
+        <p>{city}, {state}, {zipCode}, {country}</p>
+        <p>{cohort_year}</p>
+        <p>{id}</p>
+        {removeButton ? removeCard : addCard}
+        {/* <button onClick={addToArray}>add to cart</button>
+        <button id={id} onClick={removeFromArray}>Remove</button> */}
     </>
 )
 
